72de256fa01b71e9a2d170eeb8e1a575256457b0
sanu-pc <sethisanuja1998.com>
2024-08-30 23:17:47 +0530

    deployment commit

---
+++ src/constants/index.js
@@ -0,0 +1,56 @@
+export const DEFAULT_PLACE = {
+  name: 'Kolkata',
+  place_id: 'kolkata',
+  // adm_area1: 'England',
+  // adm_area2: 'Greater London',
+  country: 'India',
+  lat: '22.5744N',
+  lon: '88.3629E',
+  timezone: 'Asia/Kolkata',
+  type: 'settlement',
+};
+export const MEASUREMENT_SYSTEMS = {
+  AUTO: 'auto',
+  METRIC: 'metric',
+  UK: 'uk',
+  US: 'us',
+  CA: 'ca',
+};
+export const UNITS = {
+  metric: {
+    temperature: '°C',
+    precipitation: 'mm/h',
+    wind_speed: 'm/s',
+    visibility: 'km',
+    humidity: '%',
+    uv_index: '',
+    cloud_cover: '%',
+  },
+  us: {
+    temperature: '°F',
+    precipitation: 'in/h',
+    wind_speed: 'mph',
+    visibility: 'mi',
+    humidity: '%',
+    uv_index: '',
+    cloud_cover: '%',
+  },
+  uk: {
+    temperature: '°C',
+    precipitation: 'mm/h',
+    wind_speed: 'mph',
+    visibility: 'mi',
+    humidity: '%',
+    uv_index: '',
+    cloud_cover: '%',
+  },
+  ca: {
+    temperature: '°C',
+    precipitation: 'mm/h',
+    wind_speed: 'km/h',
+    visibility: 'km',
+    humidity: '%',
+    uv_index: '',
+    cloud_cover: '%',
+  },
+};
